7709421ed88d8a13dda6bdadabc29ba587fa5956
Armen Gadayan <armengadayan@gmail.com>
2022-02-26 17:17:20 -0800

    term

--- client/src/App.js
+++ client/src/App.js
@@ -8,32 +8,40 @@ class App extends Component {
       department: "",
       teacher: "",
       class: "",
+      term: "",
     };
 
     this.handleChange = this.handleTeacherChange.bind(this);
     this.handleDepartmentChange = this.handleDepartmentChange.bind(this);
     this.handleClassChange = this.handleClassChange.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleDepartmentChange(event) {
-    this.setState({ department: event.target.department });
+    this.setState({ department: event.target.department.value });
   }
 
   handleTeacherChange(event) {
-    this.setState({ teacher: event.target.teacher });
+    this.setState({ teacher: event.target.teacher.value });
   }
 
   handleClassChange(event) {
-    this.setState({ department: event.target.class });
+    this.setState({ classCode: event.target.class.value });
   }
+
+  handleTermChange(event) {
+    this.setState({ classCode: event.target.term.value });
+  }
+
   handleSubmit(event) {
     alert(
       "submitted all three" +
         this.state.class +
         this.state.department +
-        this.state.teacher
+        this.state.teacher +
+        this.state.term
     );
     event.preventDefault();
   }
@@ -46,29 +54,42 @@ class App extends Component {
             Department:
             <input
               type="text"
-              teacher={this.state.department}
+              department={this.state.department.value}
               onChange={this.handleDepartmentChange}
             />
           </label>
+          <br />
 
           <label>
             Teacher:
             <input
               type="text"
-              teacher={this.state.teacher}
+              teacher={this.state.teacher.value}
               onChange={this.handleChange}
             />
           </label>
-
+          <br />
           <label>
             Class:
             <input
               type="text"
-              teacher={this.state.class}
+              classCode={this.state.class.value}
               onChange={this.handleClassChange}
             />
           </label>
-          <input type="submit" department="Submit" />
+          <br />
+
+          <label>
+            Term
+            <select term={this.state.term} onChange={this.handleTermChange}>
+              <option value="2021 Fall">2021 Fall</option>
+              <option value="2022 Winter">2022 Winter</option>
+              <option value="2022 Spring">2022 Spring</option>
+            </select>
+          </label>
+
+          <br />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
