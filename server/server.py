8e4fe84027086f6f3ffdada2327c141cb12944da
Sevag Avedissian <sevag.avedissian@gmail.com>
2022-02-26 17:44:34 -0800

    added API

--- server/server.py
+++ server/server.py
@@ -8,15 +8,18 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/search", methods=['POST'])
-def members():
-    search_input = request.get_json()
+def search():
+    search_input = request.get_json()["input"]
     print(search_input)
     term = "2021 Fall"
-    dep = "I%26C SCI"
-    param = {"term" : term, "department" : dep}
-    #data = json.loads(requests.get("https://api.peterportal.org/rest/v0/schedule/soc?term="+term+"&department="+dep))
-    #uci = RateMyProfScraper(1074)
-    #uci.SearchProfessor("Alex Thornton")
+    course_number = search_input["class"]
+    department = search_input["department"]
+    instructor = search_input["teacher"]
+    url = "https://api.peterportal.org/rest/v0/schedule/soc?term="+term+"&department="+department+"&courseNumber="+course_number+"&instructorName="+instructor
+    r = requests.get(url)
+    search_result = json.loads(r.text)
+
+
     file = open("prof_dict.json")
     profs = json.load(file)
     for tid, name in profs.items():
@@ -27,7 +30,7 @@ def members():
             rating = soup.find("div", class_="RatingValue__Numerator-qw8sqy-2 liyUjw")
             print(rating.text)
     #return prof_data.text
-    return "{\"prof\": \"thornton\"}"
+    return search_result
     #return requests.get("https://api.peterportal.org/rest/v0/schedule/soc?term="+term+"&department="+dep).json()
 
 if __name__ == "__main__":
